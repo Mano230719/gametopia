ddb97f8f5e8b1456ab3854adb67a54a25f66f8c1
Mihai Manolica <mihai.manolica@gmail.com>
2024-04-04 16:30:39 +0300

    Enhanced Card Designs

--- resources/views/components/setup/setup-card.blade.php
+++ resources/views/components/setup/setup-card.blade.php
@@ -1,10 +1,12 @@
-<div class="bg-white shadow-lg rounded-lg overflow-hidden">
+<div
+    class="bg-gray-800 shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105 hover:shadow-red-200">
     <img class="w-full h-64 object-cover object-center" src="https://picsum.photos/150/100" alt="setup image">
     <div class="p-4">
-        <h3 class="text-xl font-bold mb-2">Setup Title</h3>
-        <p class="text-gray-700"><strong>Motherboard:</strong> Example Motherboard</p>
-        <p class="text-gray-700"><strong>Graphics Card:</strong> Example Graphics Card</p>
-        <p class="text-gray-700"><strong>Processor:</strong> Example Processor</p>
-        <p class="text-gray-700"><strong>RAM:</strong> 16GB DDR4</p>
+        <h3 class="text-xl text-red-500 font-bold mb-2">Setup Title</h3>
+        <hr class="border-gray-600 mt-2 mb-2 w-3/4">
+        <p class="text-white"><strong class="text-red-500">Motherboard:</strong> Example Motherboard</p>
+        <p class="text-white"><strong class="text-red-500">Graphics Card:</strong> Example Graphics Card</p>
+        <p class="text-white"><strong class="text-red-500">Processor:</strong> Example Processor</p>
+        <p class="text-white"><strong class="text-red-500">RAM:</strong> 16GB DDR4</p>
     </div>
 </div>
--- resources/views/components/shop/product-card.blade.php
+++ resources/views/components/shop/product-card.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="col-span-1 max-w-xs rounded-2xl overflow-hidden shadow-lg bg-white transition-transform transform hover:scale-105 hover:shadow-red-200">
+    class="col-span-1 max-w-xs rounded-2xl overflow-hidden shadow-lg bg-gray-800 transition-transform transform hover:scale-105 hover:shadow-red-200">
     <a href="#">
         <img src="https://picsum.photos/150/100" alt="Product Image" class="w-full h-56 object-cover">
     </a>
@@ -8,14 +8,14 @@
             <a href="#">
                 <h3 class="font-bold text-lg text-red-500 hover:text-red-600 mb-1">Product Title</h3>
             </a>
-            <p class="text-gray-700 text-sm mb-2">
+            <p class="text-gray-300 text-sm mb-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero tempora dignissimos
                 perferendis corrupti quis ut.
             </p>
         </div>
-        <hr class="my-2">
+        <hr class="border-gray-600 my-2">
         <div class="flex justify-between items-center">
-            <p class="text-gray-900 font-bold text-md">$19.99</p>
+            <p class="text-red-500 font-bold text-md">$19.99</p>
             <button
                 class="text-white bg-red-500 hover:bg-red-600 hover:text-black px-3 py-1 rounded-xl transition duration-300">
                 Add to Cart
